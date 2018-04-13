994ed8bd589c7bdcf74400811c028666bf1a948a
Andres Sarmiento Tobon <a.sarmientot@uniandes.edu.co>
2018-04-12 23:29:16 -0500

    Modificaciones

--- chat/chat.js
+++ chat/chat.js
@@ -82,6 +82,16 @@ if (Meteor.isClient) {
 		/*
 		 * END INTERACTIVE CHART
 		 */
+		 
+		   // Make the dashboard widgets sortable Using jquery UI
+		  $('.connectedSortable').sortable({
+			placeholder         : 'sort-highlight',
+			connectWith         : '.connectedSortable',
+			handle              : '.box-header, .nav-tabs',
+			forcePlaceholderSize: true,
+			zIndex              : 999999
+		  });
+		  $('.connectedSortable .box-header, .connectedSortable .nav-tabs-custom').css('cursor', 'move');
 		} catch(e) {
 			console.log(e);
 		}
