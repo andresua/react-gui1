0eb2d5310d07fa1f6490b198b1e805cbccf5bcb7
Andres Sarmiento Tobon <a.sarmientot@uniandes.edu.co>
2018-05-18 00:24:32 -0500

    Correccion de localizacion de Math.floor

--- chat/chat.js
+++ chat/chat.js
@@ -39,8 +39,8 @@ if (Meteor.isClient) {
 						
 						if(eleAvg) {
 							if(result.length > 0)
-								eleAvg.innerText = result.map(collectionItem => collectionItem.value).reduce((value1, value2) => 
-									Math.floor((typeof value1 === 'string' ? parseInt(value1) : value1) + (typeof value2 === 'string' ? parseInt(value2) : value2)) / result.length * 100) / 100.0
+								eleAvg.innerText = (Math.floor(result.map(collectionItem => collectionItem.value).reduce((value1, value2) => 
+									(typeof value1 === 'string' ? parseInt(value1) : value1) + (typeof value2 === 'string' ? parseInt(value2) : value2))) / result.length * 100) / 100.0
 										+ "%";
 							else
 								eleAvg.innerText = "0%";
